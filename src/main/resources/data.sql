b05425ad2c837c5a0506d9f85467c2dd137828df
LP Simeon <lp.simeon@yahoo.com>
2026-05-12 21:33:18 +0200

    feat: Added "Nike React Infinity Run Flyknit" shoe into data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
--- START TRANSACTION
 -- Inserimento scarpe
 INSERT INTO scarpe (nome, categoria, prezzo, descrizione, immagine_cover, nuovi_arrivi, best_seller)
 VALUES ("Nike Air Max 270", "Lifestyle", 129.99, "Le Nike Air Max 270 offrono un comfort incredibile grazie all'ammortizzazione Air Max. Ideali per l'uso quotidiano e l'attività sportiva.", "images/Shoes/air-max-270/black/air-max-270-black-1.png", true, 5);
@@ -29,4 +28,33 @@ VALUES (1, "images/Shoes/air-max-270/black/air-max-270-black-1.png"),
        (2, "images/Shoes/air-max-270/white/air-max-270-white-8.avif"),
        (2, "images/Shoes/air-max-270/white/air-max-270-white-9.avif");
 
--- COMMIT; 
+INSERT INTO scarpe (nome, categoria, prezzo, descrizione, immagine_cover, nuovi_arrivi, best_seller)
+VALUES ("Nike React Infinity Run Flyknit", "Running", 159.99, "Le Nike React Infinity Run Flyknit sono progettate per la massima ammortizzazione e stabilità durante la corsa. Con tomaia in Flyknit per un comfort superiore.", "images/Shoes/react-infinity-run-flyknit/black-yellow/react-infinity-run-flyknit-black-yellow-1.png", false, 1);
+
+-- Taglie
+INSERT INTO scarpa_taglie (scarpa_id, taglie_disponibili) VALUES (2, 38), (2, 39), (2, 40), (2, 41), (2, 42), (2, 43), (2, 44), (2, 45);
+
+-- Colori
+INSERT INTO scarpa_colori (scarpa_id, colori_disponibili) VALUES (2, "nero-giallo"), (2, "grigio"), (2, "rosso");
+
+-- Immagini colore
+INSERT INTO immagini_colore (scarpa_id, colore) VALUES (2, "nero-giallo"), (2, "grigio"), (2, "rosso");
+
+INSERT INTO url_images (img_colore_id, url)
+VALUES (3, "images/Shoes/react-infinity-run-flyknit/black-yellow/react-infinity-run-flyknit-black-yellow-1.png"),
+       (3, "images/Shoes/react-infinity-run-flyknit/black-yellow/react-infinity-run-flyknit-black-yellow-2.png"),
+       (3, "images/Shoes/react-infinity-run-flyknit/black-yellow/react-infinity-run-flyknit-black-yellow-3.png"),
+       (3, "images/Shoes/react-infinity-run-flyknit/black-yellow/react-infinity-run-flyknit-black-yellow-4.png"),
+       (3, "images/Shoes/react-infinity-run-flyknit/black-yellow/react-infinity-run-flyknit-black-yellow-5.png"),
+       (4, "images/Shoes/react-infinity-run-flyknit/grey/react-infinity-run-flyknit-grey-1.png"),
+       (4, "images/Shoes/react-infinity-run-flyknit/grey/react-infinity-run-flyknit-grey-2.png"),
+       (4, "images/Shoes/react-infinity-run-flyknit/grey/react-infinity-run-flyknit-grey-3.png"),
+       (4, "images/Shoes/react-infinity-run-flyknit/grey/react-infinity-run-flyknit-grey-4.png"),
+       (4, "images/Shoes/react-infinity-run-flyknit/grey/react-infinity-run-flyknit-grey-5.png"),
+       (4, "images/Shoes/react-infinity-run-flyknit/grey/react-infinity-run-flyknit-grey-6.png"),
+       (5, "images/Shoes/react-infinity-run-flyknit/red/react-infinity-run-flyknit-red-1.png"),
+       (5, "images/Shoes/react-infinity-run-flyknit/red/react-infinity-run-flyknit-red-2.png"),
+       (5, "images/Shoes/react-infinity-run-flyknit/red/react-infinity-run-flyknit-red-3.png"),
+       (5, "images/Shoes/react-infinity-run-flyknit/red/react-infinity-run-flyknit-red-4.png"),
+       (5, "images/Shoes/react-infinity-run-flyknit/red/react-infinity-run-flyknit-red-5.png"),
+       (5, "images/Shoes/react-infinity-run-flyknit/red/react-infinity-run-flyknit-red-6.png");
