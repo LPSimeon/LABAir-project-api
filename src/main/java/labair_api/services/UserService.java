eff3f3157b6cbc238d59a6f3ce651224cc96bdca
LP Simeon <lp.simeon@yahoo.com>
2026-06-15 23:24:48 +0200

    feat: Created SecurityConfig in order to disable csrf and to properly use encoder Bean, and created incomplete "loginUser" method

--- src/main/java/labair_api/services/UserService.java
+++ src/main/java/labair_api/services/UserService.java
@@ -2,6 +2,7 @@ package labair_api.services;
 
 import labair_api.dto.UserDTO;
 import labair_api.exceptions.ExistingUserException;
+import labair_api.exceptions.ResourceNotFoundException;
 import labair_api.models.User;
 import labair_api.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,14 +12,18 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder encoder;
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder encoder) { this.userRepository = userRepository; this.encoder = encoder; }
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
 
     public User getUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow();
     }
 
-    public UserDTO addUser(UserDTO userDTO){
-        if(userRepository.existsByEmail(userDTO.getEmail())){
+    public UserDTO addUser(UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.getEmail())) {
             throw new ExistingUserException();
         }
 
@@ -26,13 +31,21 @@ public class UserService {
         userToAdd.setNome(userDTO.getNome());
         userToAdd.setCognome(userDTO.getCognome());
         userToAdd.setEmail(userDTO.getEmail());
-        userToAdd.setPassword(userDTO.getPassword());
+        userToAdd.setPassword(encoder.encode(userDTO.getPassword()));
         userToAdd.setGiorno(userDTO.getGiorno());
         userToAdd.setMese(userDTO.getMese());
         userToAdd.setAnno(userDTO.getAnno());
 
         userRepository.save(userToAdd);
         return userDTO;
+    }
+
+    public UserDTO loginUser(UserDTO userDTO) {
+        User userFound = userRepository.findByEmail(userDTO.getEmail());
+
+        boolean loginSuccess = encoder.matches(userDTO.getPassword(), userFound.getPassword());
+
+        return convertToDTO(userFound);
     }
 
     public UserDTO convertToDTO(User user) {
@@ -45,7 +58,7 @@ public class UserService {
         convertedUser.setCognome(user.getCognome());
         convertedUser.setEmail(user.getEmail());
 
-        convertedUser.setPassword(encoder.encode(user.getPassword()));
+        convertedUser.setPassword(user.getPassword());
 
         convertedUser.setGiorno(user.getGiorno());
         convertedUser.setMese(user.getMese());
