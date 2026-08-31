94afda119488bc89c81663a7285bda545fc3e3a5
LP Simeon <lp.simeon@yahoo.com>
2026-08-31 20:54:00 +0200

    feat: Implemented JwtService with extractUsername, extractAllClaims and getSignInKey methods

--- src/main/java/labair_api/config/JwtAuthenticationFilter.java
+++ src/main/java/labair_api/config/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import labair_api.services.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Component;
@@ -14,15 +15,20 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String userEmail;
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
 
     }
 }
---
+++ src/main/java/labair_api/services/JwtService.java
@@ -0,0 +1,29 @@
+package labair_api.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+
+@Service
+public class JwtService {
+
+    // Key generated with online tool
+    private static final String SECRET_KEY = "8fa065987c70bf8448250304a7d212b5d9dba23b1a877fced8761a9b17e418bc";
+
+    public String extractUsername(String token){
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBites = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBites);
+    }
+}
