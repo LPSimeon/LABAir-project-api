511ba8c823905306c76add00aac71e529026e8a9
LP Simeon <lp.simeon@yahoo.com>
2026-08-31 20:56:32 +0200

    feat: Created extractClaim method in JwtService

--- src/main/java/labair_api/services/JwtService.java
+++ src/main/java/labair_api/services/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -16,6 +17,11 @@ public class JwtService {
 
     public String extractUsername(String token){
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token){
