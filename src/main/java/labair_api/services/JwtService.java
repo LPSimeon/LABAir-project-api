efcff0cd0ad6cad031adba9e12bd3e26ef40a33e
LP Simeon <lp.simeon@yahoo.com>
2026-08-31 21:12:40 +0200

    feat: Created isTokenValid, isTokenExpired and extractExpiration methods in JwtService

--- src/main/java/labair_api/services/JwtService.java
+++ src/main/java/labair_api/services/JwtService.java
@@ -41,6 +41,19 @@ public class JwtService {
                 .signWith(getSignInKey(), Jwts.SIG.HS256).compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(token).getPayload();
     }
