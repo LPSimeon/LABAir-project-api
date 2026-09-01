cde6e535c79bb867896def7e598de94f4d54213a
LP Simeon <lp.simeon@yahoo.com>
2026-09-01 18:02:34 +0200

    feat: Created authenticationProvider and passwordEncoder methods in ApplicationConfig

--- src/main/java/labair_api/config/ApplicationConfig.java
+++ src/main/java/labair_api/config/ApplicationConfig.java
@@ -5,8 +5,12 @@ import labair_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,5 +29,17 @@ public class ApplicationConfig {
 
             return user;
         };
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
