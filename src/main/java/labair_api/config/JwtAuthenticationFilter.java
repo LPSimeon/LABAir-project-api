891fb758a7da965f19ec851301a1bea8e2e96537
LP Simeon <lp.simeon@yahoo.com>
2026-09-01 10:12:14 +0200

    feat: added token validation, SecurityContextHolder update and filterChain call inside doFilterInternal method of JwtAuthenticationFilter

--- src/main/java/labair_api/config/JwtAuthenticationFilter.java
+++ src/main/java/labair_api/config/JwtAuthenticationFilter.java
@@ -7,9 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import labair_api.services.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -33,9 +35,26 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
+        // check if the user is not null and is not authenticated
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            // check if the token/user is still valid
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
 
+                // update SecurityContextHolder and authentication token
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            // filterChain call
+            filterChain.doFilter(request, response);
         }
     }
 }
